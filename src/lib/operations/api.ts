c7e9faac2befcb58f56dffab682efb07244e6a33
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-12-19 10:35:09 +0000

    feat: change api URL

--- src/lib/operations/api.ts
+++ src/lib/operations/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://kms-server.othent.io",
-  baseURL: "http://localhost:3001",
+  baseURL: "https://kms-server.othent.io",
 });
