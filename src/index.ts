4e6842e0bce5e3612492d4fa9d69e962c47a3d8b
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-02-16 15:49:23 +0000

    fix: change name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from "./lib/mapping/connect";
 export * from "./lib/mapping/decrypt";
 export * from "./lib/mapping/disconnect";
 export * from "./lib/mapping/encrypt";
-export * from "./lib/mapping/getActiveKey";
+export * from "./lib/mapping/getActiveAddress";
 export * from "./lib/mapping/getActivePublicKey";
 export * from "./lib/mapping/getWalletNames";
 export * from "./lib/mapping/sign";
--- src/lib/mapping/getActiveAddress.ts
+++ src/lib/mapping/getActiveAddress.ts
@@ -4,7 +4,7 @@ import { userDetails } from "../auth/userDetails";
  * Get the active wallet address of the users wallet. This function assumes (and requires) a user is logged in.
  * @returns The active wallet address of the users wallet.
  */
-export async function getActiveKey(): Promise<string> {
+export async function getActiveAddress(): Promise<string> {
   const user = await userDetails();
 
   return user.walletAddress;
