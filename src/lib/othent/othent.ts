bd2e419737b598f10c812e7325baab8cd9c12a3e
Dani Gámez Franco <gmzcodes@outlook.com>
2024-08-26 13:49:43 +0200

    Fix TSDoc typo.

--- src/lib/othent/othent.ts
+++ src/lib/othent/othent.ts
@@ -396,7 +396,7 @@ export class Othent implements Omit<ArConnect, "connect"> {
 
   /**
    * If and only if you set the [`auth0LogInMethod = "redirect"`](./constructor.md#auth0loginmethod-auth0loginmethod) option,
-   * users will be redirected to Auth0 to authenticate and then back to your application. When they land bank in your
+   * users will be redirected to Auth0 to authenticate and then back to your application. When they land back in your
    * application, you must call `completeConnectionAfterRedirect()` to complete the authentication process.
    *
    * By default, `callbackUriWithParams = location.href`, if you environment supports it. Otherwise, you'll have to manually
